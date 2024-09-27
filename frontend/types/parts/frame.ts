0d3da6e47df03dae37a3e9136a47f0871e501427
gbstorey <gbstorey@asu.edu>
2024-09-26 20:25:14 -0400

    Add prototype frame cards, frame types, dummy frame data

---
+++ frontend/types/parts/frame.ts
@@ -0,0 +1,15 @@
+export type Frame = {
+    _id: string
+    imgURL: string
+    name: string
+    priceUSD: number
+    wheelSizeInches: number
+    suspensionMM: number
+    intendedUse: string
+    available: boolean
+    sku: string
+    material: string
+    weightLbs: number
+    colors: string[]
+    size: string[]
+}
---
+++ frontend/utils/dummy.ts
@@ -0,0 +1,49 @@
+import { Frame } from "../types/parts/frame";
+
+export const frames: Frame[] = [
+    {
+        _id: "507f1f77bcf86cd799439012",
+        imgURL: "https://www.statebicycle.com/cdn/shop/files/CarbonAll-RoadFrame_ForkSet-Black_Ember_grande.jpg?v=1688596256",
+        name: "Ibis Ripley V5",
+        priceUSD: 3499.00,
+        wheelSizeInches: 29,
+        suspensionMM: 160,
+        intendedUse: "Enduro",
+        available: true,
+        sku: "RLC1GNS00000025",
+        material: 'Carbon Fiber',
+        weightLbs: 5.21,
+        colors: ["Green", "Blue"],
+        size: ["SM", "MD", "XMD", "LG"]
+    },
+    {
+        _id: "507f1f77bcf86cd799439013",
+        imgURL: "https://www.statebicycle.com/cdn/shop/files/CarbonAll-RoadFrame_ForkSet-Black_Ember_grande.jpg?v=1688596256",
+        name: "Ibis Ripley V6",
+        priceUSD: 3499.00,
+        wheelSizeInches: 29,
+        suspensionMM: 160,
+        intendedUse: "Enduro",
+        available: true,
+        sku: "RLC1GNS00000025",
+        material: 'Carbon Fiber',
+        weightLbs: 5.21,
+        colors: ["Green", "Blue"],
+        size: ["SM", "MD", "XMD", "LG"]
+    },
+    {
+        _id: "507f1f77bcf86cd799439014",
+        imgURL: "https://www.statebicycle.com/cdn/shop/files/CarbonAll-RoadFrame_ForkSet-Black_Ember_grande.jpg?v=1688596256",
+        name: "Ibis Ripley V7",
+        priceUSD: 3499.00,
+        wheelSizeInches: 29,
+        suspensionMM: 160,
+        intendedUse: "Enduro",
+        available: true,
+        sku: "RLC1GNS00000025",
+        material: 'Carbon Fiber',
+        weightLbs: 5.21,
+        colors: ["Green", "Blue"],
+        size: ["SM", "MD", "XMD", "LG"]
+    }
+]
