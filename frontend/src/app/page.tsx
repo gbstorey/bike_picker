853b0649d807a2488ee9a4a71beb4f7b96e9c081
gbstorey <gbstorey@asu.edu>
2024-09-30 20:12:28 -0400

    Implement primitive save frame to bike logic

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 export default function Home() {
 	const { userBike } = useContext(AppContext);
 	const { name, description, components } = userBike;
+
 	return (
 		<ContextProvider>
 			<main className="mx-12 hero">
@@ -24,10 +25,21 @@ export default function Home() {
 					<p className="text-4xl font-thin ml-3 mt-2">{description}</p>
 					<section
 						id="frames"
-						className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ml-2 mt-6"
+						className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow ml-2 mt-6"
 					>
 						<p className="mb-8">
-							{components.frame ? components.frame.name : "Please choose a frame!"}
+							{components.frame && (
+								<div className="space-y-4">
+									<p>{components.frame.name}</p>
+									<Image
+										src={components.frame.imgURL}
+										width={70}
+										height={70}
+										alt={`Photo of frame`}
+									/>
+								</div>
+							)}
+							{!components.frame && "Please choose a frame!"}
 						</p>
 						<CustomLink linkText="View Frames" href="/frames" />
 					</section>
--- frontend/src/components/tables/genericTable.tsx
+++ frontend/src/components/tables/genericTable.tsx
@@ -1,72 +1,91 @@
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
+import { AppContext } from "@/app/context-provider";
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { useContext } from "react";
+import { Frame } from "../../../types/parts/frame";
+import { useRouter } from "next/navigation";
 
 interface ReactTableProps<T extends object> {
-  data: T[];
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  columns: ColumnDef<T, any>[];
+	data: T[];
+	/* eslint-disable @typescript-eslint/no-explicit-any */
+	columns: ColumnDef<T, any>[];
 }
 
-export const GenericTable = <T extends object>({
-  data,
-  columns,
-}: ReactTableProps<T>) => {
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
+export const GenericTable = <T extends object>({ data, columns }: ReactTableProps<T>) => {
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	});
+	const router = useRouter();
+	const { userBike, setUserBike } = useContext(AppContext);
 
-  return (
-    <div className="flex flex-col">
-      <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="inline-block min-w-full py-4 sm:px-6 lg:px-8">
-          <div className="overflow-hidden p-2">
-            <table className="min-w-full text-center">
-              <thead className="border-b bg-gray-50">
-                {table.getHeaderGroups().map((headerGroup) => (
-                  <tr key={headerGroup.id}>
-                    {headerGroup.headers.map((header) => (
-                      <th
-                        key={header.id}
-                        className="px-6 py-4 text-xs font-medium text-gray-900"
-                      >
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                      </th>
-                    ))}
-                  </tr>
-                ))}
-              </thead>
-              <tbody>
-                {table.getRowModel().rows.map((row) => (
-                  <tr key={row.id} className='border-b" bg-white'>
-                    {row.getVisibleCells().map((cell) => (
-                      <td
-                        className="whitespace-nowrap px-3 py-4 text-xs font-light text-gray-900"
-                        key={cell.id}
-                      >
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const handleAddFrame = (e: any) => {
+		const frameID = e.target.id;
+		const frame = data[+frameID] as Frame;
+		const newBike = userBike;
+		newBike.components.frame = frame;
+		setUserBike(newBike);
+		router.push("/");
+	};
+
+	return (
+		<div className="flex flex-col">
+			<div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+				<div className="inline-block min-w-full py-4 sm:px-6 lg:px-8">
+					<div className="overflow-hidden p-2">
+						<table className="min-w-full text-center">
+							<thead className="border-b bg-gray-50">
+								{table.getHeaderGroups().map((headerGroup) => (
+									<tr key={headerGroup.id}>
+										{headerGroup.headers.map((header) => (
+											<th
+												key={header.id}
+												className="px-6 py-4 text-xs font-medium text-gray-900"
+											>
+												{header.isPlaceholder
+													? null
+													: flexRender(
+															header.column.columnDef.header,
+															header.getContext()
+													  )}
+											</th>
+										))}
+									</tr>
+								))}
+							</thead>
+							<tbody>
+								{table.getRowModel().rows.map((row) => (
+									<tr key={row.id} className='border-b" bg-white'>
+										{row.getVisibleCells().map((cell) => (
+											<td
+												className="whitespace-nowrap px-3 py-4 text-xs font-light text-gray-900"
+												key={cell.id}
+											>
+												{flexRender(
+													cell.column.columnDef.cell,
+													cell.getContext()
+												)}
+											</td>
+										))}
+										<td
+											className="whitespace-nowrap px-3 py-4 text-xs font-light text-gray-900"
+											key={row.id + "_button"}
+										>
+											<button
+												className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+												id={row.id}
+												onClick={handleAddFrame}
+											>
+												Add Frame
+											</button>
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
